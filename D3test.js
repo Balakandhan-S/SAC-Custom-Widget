f63cbfa9561ce846ad6df0aed6b2a76b1f3b146a
Balakandhan-S <44322642+Balakandhan-S@users.noreply.github.com>
2020-07-08 14:55:37 +0530

    Update D3test.js

--- D3test.js
+++ D3test.js
@@ -1,3 +1,46 @@
+(function() {
+ let tmpl = document.createElement('template');
+ tmpl.innerHTML = `
+	<div id="mydiv">Hello</div>
+    `;
+
+ customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
+
+
+  constructor() {
+   super();
+   this._shadowRoot = this.attachShadow({
+    mode: "open"
+   });
+   this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+   this._firstConnection = false;
+
+  }
+
+  //Fired when the widget is added to the html DOM of the page
+  connectedCallback() {
+   this._firstConnection = true;
+      
+  }
+   
+
+  //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+  disconnectedCallback() {
+
+  }
+
+  //When the custom widget is updated, the Custom Widget SDK framework executes this function first
+  onCustomWidgetBeforeUpdate(oChangedProperties) {
+
+  }
+
+  //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+  onCustomWidgetAfterUpdate(oChangedProperties) {
+   if (this._firstConnection) {
+	redraw();
+   }
+
+  }
 
   //When the custom widget is removed from the canvas or the analytic application is closed
   onCustomWidgetDestroy() {}
