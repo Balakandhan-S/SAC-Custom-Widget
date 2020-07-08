b1c7b41ba64a934a130975b473f72ee0fde9d69c
Balakandhan-S <44322642+Balakandhan-S@users.noreply.github.com>
2020-07-08 11:05:05 +0530

    Update D3test.js

--- D3test.js
+++ D3test.js
@@ -4,7 +4,7 @@
 	<div id="mydiv">Hello</div>
     `;
 
- customElements.define('com-sap-sample-helloworld1', class HelloWorld1 extends HTMLElement {
+ customElements.define('com-sap-sample-D3test', class D3test1 extends HTMLElement {
 
 
   constructor() {
