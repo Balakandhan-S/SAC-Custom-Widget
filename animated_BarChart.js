09dc6a2804c7f26e312f4ade159b3e7612b653f9
Balakandhan-S <44322642+Balakandhan-S@users.noreply.github.com>
2020-07-23 11:29:00 +0530

    Update animated_BarChart.js

--- animated_BarChart.js
+++ animated_BarChart.js
@@ -70,79 +70,79 @@
 	  
 			console.log("redraw...");  
 			var svg = d3.select(this.shadowRoot).append("svg")
-    .attr("width", width+margin).attr("height", height + margin).style("background-color","lightgray");
-    var xScale = d3.scaleBand().range([0, width]).padding(0.4),
-      yScale = d3.scaleLinear().range([height, 0]);
+   			.attr("width", width+margin).attr("height", height + margin).style("background-color","lightgray");
+    			var xScale = d3.scaleBand().range([0, width]).padding(0.4),
+      			yScale = d3.scaleLinear().range([height, 0]);
 
-    var g = svg.append("g")
-      .attr("transform", "translate(" + margin/2 + "," + margin/2 + ")");
+    			var g = svg.append("g")
+      			.attr("transform", "translate(" + margin/2 + "," + margin/2 + ")");
 
-    var data = [{
-        "year": 2011,
-        "value": 45
-      },
-      {
-        "year": 2012,
-        "value": 47
-      },
-      {
-        "year": 2013,
-        "value": 52
-      },
-      {
-        "year": 2014,
-        "value": 70
-      },
-      {
-        "year": 2015,
-        "value": 75
-      },
-      {
-        "year": 2016,
-        "value": 78
-      }
-    ];
-	
-    xScale.domain(data.map(function(d) {
-      return d.year;
-    }));
-    yScale.domain([0, d3.max(data, function(d) {
-      return d.value;
-    })]);
+			var data = [{
+			"year": 2011,
+			"value": 45
+			},
+			{
+			"year": 2012,
+			"value": 47
+			},
+			{
+			"year": 2013,
+			"value": 52
+			},
+			{
+			"year": 2014,
+			"value": 70
+			},
+			{
+			"year": 2015,
+			"value": 75
+			},
+			{
+			"year": 2016,
+			"value": 78
+			}
+			];
 
-    g.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(xScale));
+			xScale.domain(data.map(function(d) {
+			return d.year;
+			}));
+			yScale.domain([0, d3.max(data, function(d) {
+			return d.value;
+			})]);
 
-    g.append("g")
-      .call(d3.axisLeft(yScale).tickFormat(function(d) {
-        return "$" + d;
-      }).ticks(10));
+			g.append("g")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(xScale));
+
+			g.append("g")
+			.call(d3.axisLeft(yScale).tickFormat(function(d) {
+			return "$" + d;
+			}).ticks(10));
 
 
-    g.selectAll(".bar")
-      .data(data)
-      .enter().append("rect")
-      .attr("class", "bar")
-      .attr("style", "fill:steelblue")
-      .attr("x", function(d) {
-        return xScale(d.year);
-      })
-      .attr("y", function(d) {
-        return yScale(d.value);
-      })
-      .attr("width", xScale.bandwidth())
-      .attr("height", function(d) {
-        return height - yScale(d.value);
-      })
-      .on("mouseover", function() {
-        d3.select(this)
-          .style("fill", "orange");
-      })
-      .on("mouseout", function() {
-        d3.select(this)
-          .style("fill", "steelblue")
-      });
+			g.selectAll(".bar")
+			.data(data)
+			.enter().append("rect")
+			.attr("class", "bar")
+			.attr("style", "fill:steelblue")
+			.attr("x", function(d) {
+			return xScale(d.year);
+			})
+			.attr("y", function(d) {
+			return yScale(d.value);
+			})
+			.attr("width", xScale.bandwidth())
+			.attr("height", function(d) {
+			return height - yScale(d.value);
+			})
+			.on("mouseover", function() {
+			d3.select(this)
+			  .style("fill", "orange");
+			})
+			.on("mouseout", function() {
+			d3.select(this)
+			  .style("fill", "steelblue")
+			});
 
 
 		}
