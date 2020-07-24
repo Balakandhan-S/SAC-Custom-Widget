b0b57aa48d3b8d305372f605d1af211c5868ce56
Balakandhan-S <44322642+Balakandhan-S@users.noreply.github.com>
2020-07-24 17:09:50 +0530

    Update custom_BarChart.js

--- custom_BarChart.js
+++ custom_BarChart.js
@@ -115,7 +115,8 @@
       			.attr("transform", "translate(" + margin/2 + "," + margin/2 + ")");
 
 			var data = [{"year": 2011,"value": 45},{"year": 2012,"value": 47},{"year": 2013,"value": 52},{"year": 2014,"value": 70},{"year": 2015,"value": 75},{"year": 2016,"value": 78}];
-
+			console.log("data var: "+data);
+			console.log("datapassed: "+this._datajson);
 			xScale.domain(data.map(function(d) {
 			return d.year;
 			}));
