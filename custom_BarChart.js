7dfef9b51a48d6acfe73bd41484b380005b0bdcb
Balakandhan-S <44322642+Balakandhan-S@users.noreply.github.com>
2020-07-24 15:01:18 +0530

    Update custom_BarChart.js

--- custom_BarChart.js
+++ custom_BarChart.js
@@ -114,7 +114,7 @@
     			var g = svg.append("g")
       			.attr("transform", "translate(" + margin/2 + "," + margin/2 + ")");
 
-			var data = this._datajson;
+			var data = [{"year": 2011,"value": 45},{"year": 2012,"value": 47},{"year": 2013,"value": 52},{"year": 2014,"value": 70},{"year": 2015,"value": 75},{"year": 2016,"value": 78}];
 
 			xScale.domain(data.map(function(d) {
 			return d.year;
